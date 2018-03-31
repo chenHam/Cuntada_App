a31b493096e24cfe5bb4aa2bf43ddbb6e15092e0
ChenHam <Chen@DESKTOP-8HEVHG2>
2018-04-01 00:38:27 +0300

    initial login activity

--- app/src/main/java/com/example/chen/cuntada_app/app/MainActivity.java
+++ app/src/main/java/com/example/chen/cuntada_app/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.chen.cuntada_app.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
