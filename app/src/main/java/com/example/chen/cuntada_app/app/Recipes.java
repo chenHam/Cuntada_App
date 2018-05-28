a4aa170a242b8154ad7cb46be99fabbf0368415f
OrtalYona <ortalyona94@gmail.com>
2018-05-28 22:30:45 +0300

    recipe

--- app/src/main/java/com/example/chen/cuntada_app/app/Recipes.java
+++ app/src/main/java/com/example/chen/cuntada_app/app/Recipes.java
@@ -2,14 +2,32 @@ package com.example.chen.cuntada_app.app;
 
 import android.widget.Spinner;
 
+import java.util.HashMap;
+
 public class Recipes {
 
     String RecipeName;
     String Ingredients;
     String Preparation;
-    Spinner Category;
+    String Category;
 
+    public Recipes(String name, String ingredients,String preparation, String category){
 
+        RecipeName = name;
+        Ingredients = ingredients;
+        Preparation = preparation;
+        Category = category;
+
+    }
+
+    HashMap<String,Object> toJson(){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("RecipeName", RecipeName);
+        result.put("Ingredients", Ingredients);
+        result.put("Preparation", Preparation);
+        result.put("Category", Category);
+        return result;
+    }
 
 
 
