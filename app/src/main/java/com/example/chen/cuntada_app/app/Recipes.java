4363e3c6ba9900d07580ea04131813542fa4e690
OrtalYona <ortalyona94@gmail.com>
2018-05-28 21:31:43 +0300

    recipes

---
+++ app/src/main/java/com/example/chen/cuntada_app/app/Recipes.java
@@ -0,0 +1,16 @@
+package com.example.chen.cuntada_app.app;
+
+import android.widget.Spinner;
+
+public class Recipes {
+
+    String RecipeName;
+    String Ingredients;
+    String Preparation;
+    Spinner Category;
+
+
+
+
+
+}
--- app/src/main/java/com/example/chen/cuntada_app/app/ShowRecipes.java
+++ app/src/main/java/com/example/chen/cuntada_app/app/ShowRecipes.java
@@ -12,38 +12,42 @@ import org.w3c.dom.Text;
 
 
 public class ShowRecipes extends Fragment {
-    private Text ingredients, preparation;
-    Button saveButton;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_add_recipes, container, false);
 
-        super.onCreate(savedInstanceState);
 
-        if (savedInstanceState == null) {
-            ShowRecipes fragment = new ShowRecipes();
-            FragmentTransaction tran = getFragmentManager().beginTransaction();
-            tran.add(R.id.main_container, fragment);
-            tran.commit();
-        }
 
-        saveButton = (Button)view.findViewById(R.id.button_save);
-        saveButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                SaveRecipeButton(view);
-            }
-        });
-        return view;
-
-    }
-
-    protected void SaveRecipeButton(View view){
-        String ingredients = view.findViewById(R.id.ingredients).toString();
-        String preparation = view.findViewById(R.id.preparation).toString();
-        //TODO: need to save to DB?
-    }
+//    private Text ingredients, preparation;
+//    Button saveButton;
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        View view = inflater.inflate(R.layout.fragment_add_recipes, container, false);
+//
+//        super.onCreate(savedInstanceState);
+//
+//        if (savedInstanceState == null) {
+//            ShowRecipes fragment = new ShowRecipes();
+//            FragmentTransaction tran = getFragmentManager().beginTransaction();
+//            tran.add(R.id.main_container, fragment);
+//            tran.commit();
+//        }
+//
+//        saveButton = (Button)view.findViewById(R.id.button_save);
+//        saveButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                SaveRecipeButton(view);
+//            }
+//        });
+//        return view;
+//
+//    }
+//
+//    protected void SaveRecipeButton(View view){
+//        String ingredients = view.findViewById(R.id.ingredients).toString();
+//        String preparation = view.findViewById(R.id.preparation).toString();
+//        //TODO: need to save to DB?
+//    }
 }
